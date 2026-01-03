03b2a3b191f9df2a94d0ae8c792b8af30709554e
Rohit <Rohitkushwah1703@gmail.com>
2026-01-03 23:25:48 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import NavBar from "./components/NavBar";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import SignUp from "./components/SignUp";
+
 function App() {
   return (
     <>
-      <div>
-        <h1>Hellow </h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Body />}>
+            <Route path={"/login"} element={<Login />} />
+            <Route path={"/profile"} element={<Profile />} />
+            <Route path={"/signup"} element={<SignUp />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NavBar from "./NavBar";
+import { Outlet } from "react-router";
+import Footer from "./Footer";
+
+const Body = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
